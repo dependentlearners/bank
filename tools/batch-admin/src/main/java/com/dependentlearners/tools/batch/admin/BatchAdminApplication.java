61f02ebada316fc8c8268b9de15a5db53bf054fa
Ankit <ankit.java8@gmail.com>
2017-09-06 23:34:47 +0200

    Initial structure of microservices on bank

---
+++ tools/batch-admin/src/main/java/com/dependentlearners/tools/batch/admin/BatchAdminApplication.java
@@ -0,0 +1,12 @@
+package com.dependentlearners.tools.batch.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchAdminApplication.class, args);
+    }
+}
